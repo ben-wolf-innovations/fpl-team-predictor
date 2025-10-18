c6c4589b7c7950b59343961b84b8b78b14e4c327
ben-wolf-innovations <ben@wolfinnovations.org>
2025-10-18 17:57:31 +0100

    Add files via upload

--- tests/test_ingestion_utils.py
+++ tests/test_ingestion_utils.py
@@ -28,7 +28,7 @@ def spark():
         .master("local[*]") \
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
+        .config("spark.jars.packages", "io.delta:delta-core_2.13:2.4.0") \
         .getOrCreate()
     
     yield spark
