fe0e0a0bbc203937c9a9cc8db6f76be536d11aa0
Ben Wolfenden <ben@wolfinnovations.org>
2025-10-18 17:08:13 +0100

    ENV and PROTOCOL in notebook

--- tests/test_ingestion_utils.py
+++ tests/test_ingestion_utils.py
@@ -21,11 +21,19 @@ class MockDbutils:
     fs = FS()
 
 @pytest.fixture(scope="session")
+
 def spark():
-    return SparkSession.builder \
-        .appName("TestReadLatestRawJson") \
+    spark = SparkSession.builder \
+        .appName("TestSession") \
         .master("local[*]") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
         .getOrCreate()
+    
+    yield spark
+    spark.stop()
+
 
 def test_read_latest_raw_json(spark, tmp_path):
     # Create mock JSON file in latest folder
